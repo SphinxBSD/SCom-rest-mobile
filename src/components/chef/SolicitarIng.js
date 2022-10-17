8630d2f43070afacca58b1edf7cfd2e959b19eb7
Andres Velarde L <andresjohnvelarde41421356@gmail.com>
2022-10-17 14:45:29 -0400

    solicitudes

--- src/components/chef/SolicitarIng.js
+++ src/components/chef/SolicitarIng.js
@@ -1,57 +1,177 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Alert, Modal, StyleSheet, Text, Pressable, View, TextInput  } from "react-native";
 import settings from '../../core/settings.json';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Buttonc from './Botonnn';
 
+
+// vectores nuevos ingredientes
+var arrayAmountN   = new Array();
+var arrayName   = new Array();
+var arrayPrice   = new Array();
+var arrayStock   = new Array();
+var tamnew = 0;
+// vectores ingredientes existentes
+var arrayId  = new Array();
+var arrayAmount = new Array();
+var tamexs = 0;
+
+
+const alertConfirmado = () =>{
+Alert.alert(
+  "Éxito",
+  "Se agregó correctamente a la solicitud",
+  [
+   
+    { text: "OK", onPress: () => console.log("OK Pressed") }
+  ]
+  
+);
+console.log("confirm")
+}
+
+const yaExiste = () =>{
+  Alert.alert(
+    "Error",
+    "Ya existe este ingrediente, solicitelo desde los existentes",
+    [
+      
+      { text: "OK", onPress: () => console.log("OK Pressed") }
+    ]
+    
+  );
+  console.log("error rep")
+  }
+
+  const noExiste = () =>{
+    Alert.alert(
+      "Error",
+      "No existe este ingrediente, solicitelo desde los nuevos",
+      [
+       
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+      
+    );
+    console.log("error inex")
+    }
+
+
+
 const SolicitarIng = (props) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [ onChangeText] = React.useState(null);
 
-  var arrayId  = new Array();
-  var arrayAmount = new Array();
-
-  var arrayAmountN   = new Array();
-  var arrayName   = new Array();
-  var arrayPrice   = new Array();
-  var arrayStock   = new Array();
+  const p = settings.puerto;
+  const u = settings.url;
+  const [ingred,setingred] = useState([])
+  useEffect(() => {
+     obtenerDatos()
+  },[]);
   
-  arrayId[0] = 1;
-  arrayId[1] = 2;
 
   
-  arrayAmount[0] = 2;
-  arrayAmount[1] = 2;
+  const obtenerDatos= async() =>
+  {
+    const data2 = await fetch(u+p+'/api/ingredients')
+    const ingredP = await data2.json()
+    setingred(ingredP)
+  }
+  var arrayNameOrg   = new Array();
+  {
+    ingred.map((ing) => (
+
+arrayNameOrg.push(ing.name) 
+    ))
+  }
+  console.log(arrayNameOrg);
+
+
+
+ 
+
+ const [namess, setNamess] = useState({ value: "" });
+ const [amountNew, setAmountNew] = useState({ value: "" });
+ const [priceNew, setPriceNew] = useState({ value: "" });
+
+ const [nameOld, setNameOld] = useState({ value: "" });
+ const [amountOld, setAmountOld] = useState({ value: "" });
+ 
+
+ const addNew = async () => {
+
+  if(arrayNameOrg.includes(namess.value))
+  {
+    yaExiste()
+  }
+else
+{
+  arrayName.push(namess.value)
+  arrayAmountN.push(amountNew.value)
+  arrayPrice.push(priceNew.value);
+  arrayStock.push(0);
+  tamnew++
+  console.log(arrayName)
+  console.log(arrayAmountN)
+  console.log(arrayPrice)
+  console.log(arrayStock)
+
+  alertConfirmado()
+  setModalVisible(!modalVisible)
+}
+
+}
+
+const addOld = async () => {
+
+  if(arrayNameOrg.includes(nameOld.value))
+  {
+    var ideAnt = arrayNameOrg.indexOf(nameOld.value)
+    ideAnt++
+    arrayId.push(ideAnt);
+    arrayAmount.push(amountOld.value)
+   
+    tamexs++
+    console.log(arrayAmount)
+    console.log(arrayId)
  
   
-  arrayAmountN[0] = 500;
-  arrayName[0] = "aba";
-  arrayPrice[0] = 13.49;
-  arrayStock[0] = 0;
-
-  var ingredients  = [];
-  var newIngredients  = [];
-  var objeto = {};
-  
-  for(var i= 0; i < arrayId.length; i++) {
-  
-    ingredients.push({ 
-      "amount"  : arrayAmount[i],
-          "id"    : arrayId[i]  
-      });
+    alertConfirmado()
+    setModalVisible(!modalVisible)
+    
   }
-  for(var i= 0; i < arrayAmountN.length; i++) {
-  
-    newIngredients.push({ 
-       
-         "amount"    : arrayAmountN[i],
-         "name"    : arrayName[i],
-         "price"    : arrayPrice[i],
-         "stock"    : arrayStock[i],
-     });
- }
+else
+{
+  noExiste()
+}
+
+}
 
   const solicitarIngredientes = async() =>{
+
+
+    var ingredients  = [];
+    var newIngredients  = [];
+    var objeto = {};
+    
+    for(var i= 0; i < arrayId.length; i++) {
+    
+      ingredients.push({ 
+        "amount"  : arrayAmount[i],
+            "id"    : arrayId[i]  
+        });
+    }
+    for(var i= 0; i < tamnew; i++) {
+    
+      newIngredients.push({ 
+         
+           "amount"    : arrayAmountN[i],
+           "name"    : arrayName[i],
+           "price"    : arrayPrice[i],
+           "stock"    : arrayStock[i],
+       });
+   }
+
 const idChef = await AsyncStorage.getItem("id");
 
 
@@ -59,6 +179,7 @@ const idChef = await AsyncStorage.getItem("id");
   objeto.ingredients = ingredients;
   objeto.newIngredients = newIngredients;
 
+  console.log(objeto);
   const pruebaJson = JSON.stringify(objeto)
   console.log(pruebaJson);
 
@@ -83,8 +204,19 @@ const idChef = await AsyncStorage.getItem("id");
          routes: [{ name: vista }],
        });
      
-   
-     
+       for(var i= 0; i < tamnew; i++) {
+          arrayAmountN.pop()
+          arrayName.pop()
+          arrayPrice.pop()
+          arrayStock.pop() 
+     }
+
+     for(var i= 0; i < tamexs; i++) {
+      arrayAmount.pop()
+      arrayId.pop()
+ }
+     tamnew=0
+     tamexs=0
   }
 
  
@@ -107,29 +239,40 @@ const idChef = await AsyncStorage.getItem("id");
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Text style={styles.modalText2}>Solicitar nuevo ingrediente</Text>
-            <TextInput style={styles.input} onChangeText={onChangeText}  placeholder="Ingrediente"/>
+
+            <TextInput style={styles.input}  onChangeText={(text) => setNamess({ value: text })}
+            placeholder="Ingrediente"/>
             <Text style={styles.modalText}>Nombre</Text>
-            <TextInput style={styles.input} onChangeText={onChangeText}  placeholder="Precio"/>
+
+
+            <TextInput style={styles.input}  keyboardType="numeric" onChangeText={(text) => setPriceNew ({ value: text })} placeholder="Precio"/>
             <Text style={styles.modalText}>Precio</Text>
-            <TextInput style={styles.input}  keyboardType="numeric" onChangeText={onChangeText}  placeholder="Cantidad"/>
+
+
+            <TextInput style={styles.input}  keyboardType="numeric" onChangeText={(text) => setAmountNew({ value: text })} placeholder="Cantidad"/>
             <Text style={styles.modalText} >Cantidad</Text>
+
             <Pressable
               style={[styles.button, styles.buttonClose2]}
-              onPress={() => setModalVisible(!modalVisible)}
+              onPress={addNew}
             
             >
-              <Text style={styles.textStyle}>Añadir</Text>
+              <Text style={styles.textStyle}>Solicitar</Text>
             </Pressable>
+
             <Text style={styles.textStyle}>{"\n"}</Text>
 
             <Text style={styles.modalText2}>Solicitar ingrediente existente</Text>
-            <TextInput style={styles.input} onChangeText={onChangeText}  placeholder="Ingrediente"/>
+
+            <TextInput style={styles.input} onChangeText={(text) => setNameOld ({ value: text })}  placeholder="Ingrediente"/>
             <Text style={styles.modalText}>Nombre</Text>
-            <TextInput style={styles.input}  keyboardType="numeric" onChangeText={onChangeText}  placeholder="Cantidad"/>
+
+            <TextInput style={styles.input}  keyboardType="numeric" onChangeText={(text) => setAmountOld ({ value: text })} placeholder="Cantidad"/>
             <Text style={styles.modalText} >Cantidad</Text>
+
             <Pressable
               style={[styles.button, styles.buttonClose2]}
-              onPress={() => setModalVisible(!modalVisible)}
+              onPress={addOld}
             
             >
               <Text style={styles.textStyle}>Solicitar</Text>
@@ -157,7 +300,9 @@ const idChef = await AsyncStorage.getItem("id");
       />
        
 
-      <Text style={styles.modalText2}>{"\n"}Lista de ingredientes solicitados{"\n"}{"\n"}</Text>
+      <Text style={styles.modalText2}>{"\n"}Lista de ingredientes nuevos solicitados{"\n"}{"\n"}</Text>
+
+      <Text style={styles.modalText2}>{"\n"}Lista de ingredientes existentes solicitados{"\n"}{"\n"}</Text>
 
       <Buttonc
         
